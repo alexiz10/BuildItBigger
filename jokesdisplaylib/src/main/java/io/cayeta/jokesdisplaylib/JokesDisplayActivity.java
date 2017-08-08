e563d5c49fdd1e845910e6e698adc7bee443baaf
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2017-08-07 21:47:27 -0700

    feat: Added functionality to JokesDisplayActivity
    Added functionality to JokesDisplayActivity in the
    jokesdisplaylib Android library so that when the Activity
    is launched, it will display the joke that was passed into it
    via intent in a TextView at the center of the Activity.

--- jokesdisplaylib/src/main/java/io/cayeta/jokesdisplaylib/JokesDisplayActivity.java
+++ jokesdisplaylib/src/main/java/io/cayeta/jokesdisplaylib/JokesDisplayActivity.java
@@ -2,12 +2,23 @@ package io.cayeta.jokesdisplaylib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class JokesDisplayActivity extends AppCompatActivity {
+
+    private static final String JOKE_EXTRA_KEY = "joke";
+
+    TextView mJokeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jokes_display);
+
+        mJokeTextView = (TextView) findViewById(R.id.tv_joke);
+
+        String joke = getIntent().getStringExtra(JOKE_EXTRA_KEY);
+        mJokeTextView.setText(joke);
     }
+
 }
