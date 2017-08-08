d91a1155bdc75cc09ad61f2cc27045976e26cb73
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2017-08-07 21:41:01 -0700

    feat: Added Activity that will display jokes

---
+++ jokesdisplaylib/src/main/java/io/cayeta/jokesdisplaylib/JokesDisplayActivity.java
@@ -0,0 +1,13 @@
+package io.cayeta.jokesdisplaylib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JokesDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes_display);
+    }
+}
