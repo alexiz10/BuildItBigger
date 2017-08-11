a2f5aa946f6932184f6152533c2ac9697c822efc
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2017-08-11 09:16:50 -0700

    test: Implemented test code
    Added code to test if joke received
    was null or not

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,7 +1,20 @@
 package com.udacity.gradle.builditbigger;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
 public class EndpointsAsyncTaskTest {
 
+    @Test
+    public void testAsyncTask() {
+        try {
+            String joke = new EndpointAsyncTask().execute().get();
 
+            assertNotNull(joke);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
